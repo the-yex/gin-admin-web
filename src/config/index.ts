55422419b7accc36dc9657e20882a96b8e8fb709
zouyuxi <zouyx@knownsec>
2025-11-18 14:02:09 +0800

    修改Ui

--- src/config/index.ts
+++ src/config/index.ts
@@ -38,7 +38,7 @@ import { headerBarConfig } from './modules/headerBar'
 const appConfig: SystemConfig = {
   // 系统信息
   systemInfo: {
-    name: 'Template design' // 系统名称
+    name: 'gin admin web' // 系统名称
   },
   // 系统主题
   systemThemeStyles: {
